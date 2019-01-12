6220957213b13d17de30a3b5c5d013e8ccb506d8
Roberto Vite <roberto.vite@hotmail.com>
2019-01-12 13:02:54 -0600

    Creación de Páginas y Tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'lista', loadChildren: './lista/lista.module#ListaPageModule' },
+  { path: 'favs', loadChildren: './favs/favs.module#FavsPageModule' },
+  { path: 'datos', loadChildren: './datos/datos.module#DatosPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
